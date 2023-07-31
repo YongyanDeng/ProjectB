42a03abb7a6920dc23e5424f58def36d130b76bb
Yongyan Deng <deng.yo@northeastern.edu>
2023-07-31 11:59:02 -0400

    update hr->profile detail page

--- client/src/pages/HrProfileDetail/index.jsx
+++ client/src/pages/HrProfileDetail/index.jsx
@@ -1,20 +1,67 @@
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { Form, Input, Table, Button, Select, Space, message } from "antd";
+import { FilePdfOutlined, MailOutlined } from "@ant-design/icons";
 
 import { getProfileDetail } from "app/hrSlice";
 import OnboardingForm from "pages/EmployeeOnboardingApplication/OnboardingForm";
+
+const visaProcess = ["OPT Receipt", "OPT EAD", "I-983", "I-20"];
 
 export default function HrProfileDetail() {
     const dispatch = useDispatch();
     const { employeeId } = useParams();
     const { employee } = useSelector((state) => state.employee);
-    const { selectedEmployee } = useSelector((state) => state.hr);
+    const { selectedEmployee, status } = useSelector((state) => state.hr);
+    const [detail, setDetail] = useState(null);
     const title = "Persoanl Profile (View Only)";
 
     useEffect(() => {
         dispatch(getProfileDetail({ id: employee.id, employeeId }));
     }, []);
+
+    useEffect(() => {
+        if (status === "successed" && !!Object.keys(selectedEmployee).length) {
+            const docs = selectedEmployee.documents.map((document, index) => {
+                return {
+                    key: index + 1,
+                    name: document.document_name,
+                    content: document.content.data,
+                    type: document.document_type,
+                    status: document.document_status,
+                };
+            });
+
+            let next_step = null;
+            const last = selectedEmployee.documents[selectedEmployee.documents.length - 1];
+            if (last?.document_status !== "approved") {
+                next_step = visaProcess[selectedEmployee.documents.length - 1];
+            } else if (selectedEmployee.documents.length < 4) {
+                next_step = visaProcess[selectedEmployee.documents.length];
+            }
+
+            setDetail({
+                name: `${selectedEmployee.name.first_name} ${selectedEmployee.name.last_name}`,
+                work_authorization: selectedEmployee.work_authorization,
+                documents: docs,
+                next_step,
+            });
+        }
+    }, [selectedEmployee]);
+
+    // handle pdf preview in new window
+    const handlePreview = (content) => {
+        // Data transfer: Buffer -> Blob
+        const uint8Array = new Uint8Array(content);
+        const pdfBlob = new Blob([uint8Array], { type: "application/pdf" });
+
+        // Open the PDF in a new window or tab
+        const pdfUrl = URL.createObjectURL(pdfBlob);
+        window.open(pdfUrl, "_blank");
+        // Revoke the object URL after use to release memory
+        URL.revokeObjectURL(pdfUrl);
+    };
 
     const fields = [
         // Name
@@ -288,5 +335,43 @@ export default function HrProfileDetail() {
         },
     ];
 
-    return <OnboardingForm title={title} data={selectedEmployee} fields={fields} />;
+    const columns = [
+        {
+            title: "Document name",
+            dataIndex: "name",
+            key: "name",
+        },
+        {
+            title: "Document type",
+            dataIndex: "type",
+            key: "type",
+        },
+        {
+            title: "Document status",
+            dataIndex: "status",
+            key: "status",
+        },
+        {
+            title: "",
+            dataIndex: "content",
+            key: "url",
+            render: (content) => {
+                return (
+                    <Button type="link" onClick={() => handlePreview(content)}>
+                        <FilePdfOutlined style={{ fontSize: "25px", color: "red" }} />
+                    </Button>
+                );
+            },
+        },
+    ];
+
+    return (
+        <div>
+            <Form>
+                <Form.Item>
+                    <Table dataSource={detail?.documents} columns={columns} />
+                </Form.Item>
+            </Form>
+        </div>
+    );
 }
